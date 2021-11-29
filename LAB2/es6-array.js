1a7276b2305eabf08379a75879ea69a1f4f0d3b8
JUTAMAT630112418027 <630112418027@bru.ac.th>
2021-11-29 12:04:04 +0700

    LAB2

---
+++ LAB2/es6-array.js
@@ -0,0 +1,23 @@
+const cars = ["Toyota","Honda","Mazda"];
+const fruits = [];
+fruits [0] = "apple";
+fruits [3] = "mango";
+const rname = new Array("Win","Jonh","james");
+console.log(fruits);
+
+const carPop = cars.pop();
+console.log(carPop);
+console.log(cars);
+
+cars.push("Kubota");
+console.log(cars);
+
+const cars.shift = cars.shift();
+console.log(carShift);
+console.log(cars);
+
+cars.unshift ("Mazda");
+console.log(cars);
+
+const slideArr = cars.shift.slide(0, 2);
+console.log(slideArr);
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Miss Jutamat Prasertsawat  
+
+## Idstudent 443020183-4  
+
+**Class Infoemation Technology**  
+**Term _2_ Education _2564_**  
+
+Coures 4132202 Internet Technology  
+
+**Link**  
+[LAB1](https://github.com/JUTAMAT630112418027/4132202-2-64/tree/main/LAB1)  
+
+**Clone**  
+git clone https://github.com/JUTAMAT630112418027/4132202-2-64.git  
